7dcbc31d459dac47ac43950284fa0722b390bb0e
wanderson-rpf <wandersonrpf@gmail.com>
2023-04-26 10:07:06 -0300

    :construction: [progress] Req 01: Create model and interface.

---
+++ src/Models/CarsODM.ts
@@ -0,0 +1,25 @@
+import { Model, Schema, model, models } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+export default class CarsODM {
+  private _schema: Schema;
+  private _model: Model<ICar>;
+
+  constructor() {
+    this._schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+
+    this._model = models.Cars || model('Cars', this._schema);
+  }
+
+  public async createCar(car: ICar): Promise<ICar> {
+    return this._model.create({ ...car });
+  }
+}
